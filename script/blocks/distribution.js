30368a6546915aa310742c29bc06f116264a3f78
Nadaadizer9611 <67803943+Nadaadizer9611@users.noreply.github.com>
2022-10-14 16:13:38 -0300

    Add files via upload

---
+++ script/blocks/distribution.js
@@ -0,0 +1,6 @@
+const slagalloyStackConveyor = extend(StackConveyor, "slag-alloy-stack-conveyor", {
+	speed: 6 / 60
+});
+const energizedplastaniumstackconveyor = extend(StackConveyor, "energized-plastanium-stack-conveyor", {
+	speed: 4 / 80
+});
